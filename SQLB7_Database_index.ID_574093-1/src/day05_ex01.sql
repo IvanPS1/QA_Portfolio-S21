f74556d96c2c748221271261439984b2827fb0bc
IvanPS1 <ivan-pankratov43@mail.ru>
2025-11-16 22:26:45 +0300

    Add files via upload

---
+++ SQLB7_Database_index.ID_574093-1/src/day05_ex01.sql
@@ -0,0 +1,16 @@
+SELECT 
+    m.pizza_name,
+    pz.name as pizzeria_name
+FROM menu m
+JOIN pizzeria pz ON m.pizzeria_id = pz.id;
+
+SET enable_seqscan = off;
+
+EXPLAIN ANALYZE 
+SELECT 
+    m.pizza_name,
+    pz.name as pizzeria_name
+FROM menu m
+JOIN pizzeria pz ON m.pizzeria_id = pz.id;
+ 
+SET enable_seqscan = on;
---
+++ SQLB7_Database_index.ID_574093-1/src/day05_ex04.sql
@@ -0,0 +1,5 @@
+CREATE UNIQUE INDEX idx_menu_unique ON menu (pizzeria_id, pizza_name);
+SET enable_seqscan = off;
+EXPLAIN ANALYZE
+SELECT DISTINCT pizzeria_id, pizza_name FROM menu;
+SET enable_seqscan = on;
